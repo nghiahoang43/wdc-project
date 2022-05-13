8ea2763e16a66ba42d29cc51a6b66c287e412f04
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-05-13 01:58:58 +0000

    Fri, May 13, 2022, 1:58 AM +00:00

--- calendar.js
+++ calendar.js
@@ -28,7 +28,7 @@ const chooseDate = (id) => {
 
   let date = document.getElementById(id);
   date.classList.toggle('chosen-day');
-}
+};
 
 // CHECK LEAP YEAR
 const isLeapYear = (year) => {
@@ -50,62 +50,62 @@ month_picker.onclick = () => {
 };
 
 const generateCalendar = (month, year) => {
-  var calendar_days = document.querySelector('.calendar-days')
-  calendar_days.innerHTML = ''
-  let calendar_header_year = document.querySelector('#year')
+  var calendar_days = document.querySelector('.calendar-days');
+  calendar_days.innerHTML = '';
+  let calendar_header_year = document.querySelector('#year');
 
-  let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  let currDate = new Date()
+  let currDate = new Date();
 
-  month_picker.innerHTML = month_names[month]
-  calendar_header_year.innerHTML = year
+  month_picker.innerHTML = month_names[month];
+  calendar_header_year.innerHTML = year;
 
-  let first_day = new Date(month, year, 1)
+  let first_day = new Date(month, year, 1);
 
   for (var i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
-    let day = document.createElement('div')
+    let day = document.createElement('div');
     if (i >= first_day.getDay()) {
-      let date = (i - first_day.getDay() + 1).toString() + (month + 1).toString() + (year).toString()
+      let date = (i - first_day.getDay() + 1).toString() + (month + 1).toString() + (year).toString();
       day.setAttribute('id', date);
       day.setAttribute('onclick', 'chooseDate(' + date + ')');
-      day.classList.add('calendar-day-hover')
-      day.innerHTML = i - first_day.getDay() + 1
+      day.classList.add('calendar-day-hover');
+      day.innerHTML = i - first_day.getDay() + 1;
       if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
-        day.classList.add('curr-date')
+        day.classList.add('curr-date');
       }
     }
-    calendar_days.appendChild(day)
+    calendar_days.appendChild(day);
   }
-}
+};
 
-let month_list = calendar.querySelector('.month-list')
+let month_list = calendar.querySelector('.month-list');
 
 month_names.forEach((e, index) => {
-  let month = document.createElement('div')
-  month.innerHTML = '<div>' + e + '</div>'
+  let month = document.createElement('div');
+  month.innerHTML = '<div>' + e + '</div>';
   month.onclick = () => {
-    month_list.classList.remove('show')
-    curr_month.value = index
-    generateCalendar(curr_month.value, curr_year.value)
-  }
-  month_list.appendChild(month)
+    month_list.classList.remove('show');
+    curr_month.value = index;
+    generateCalendar(curr_month.value, curr_year.value);
+  };
+  month_list.appendChild(month);
 })
 
 document.querySelector('#prev-year').onclick = () => {
-  --curr_year.value
-  generateCalendar(curr_month.value, curr_year.value)
+  --curr_year.value;
+  generateCalendar(curr_month.value, curr_year.value);
 }
 
 document.querySelector('#next-year').onclick = () => {
-  ++curr_year.value
-  generateCalendar(curr_month.value, curr_year.value)
+  ++curr_year.value;
+  generateCalendar(curr_month.value, curr_year.value);
 }
 
-let currDate = new Date()
+let currDate = new Date();
 
-let curr_month = { value: currDate.getMonth() }
-let curr_year = { value: currDate.getFullYear() }
+let curr_month = { value: currDate.getMonth() };
+let curr_year = { value: currDate.getFullYear() };
 
-generateCalendar(curr_month.value, curr_year.value)
+generateCalendar(curr_month.value, curr_year.value);
 

fffe401a362f692c535cbefb88230dfa22da4a47
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-05-13 10:42:04 +0930

    first init

---
+++ invitation.js
@@ -0,0 +1,7 @@
+let copyText = document.querySelector(".copy-text");
+copyText.querySelector("button").addEventListener("click", function(){
+  copyText.classList.add("active")
+  setTimeout(function() {
+    copyText.classList.remove("active")
+  }, 1000)
+})
